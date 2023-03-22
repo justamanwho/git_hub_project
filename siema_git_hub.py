155dfed713fa27a9037b0c0da7d0509495035516
Dmytro Kostiuk <dmytro.kostuyk@gmail.com>
2023-03-22 23:20:09 +0100

    adding recursive function that finds factorial

--- siema_git_hub.py
+++ siema_git_hub.py
@@ -1,5 +1,5 @@
-# Hello Github!
-# In this code I'll just print some programs to see how it's gonna look like in Github
+# Hello GitHub!
+# In this code I'll just print some programs to see how it's gonna look like in GitHub
 
 import random as r
 
@@ -8,3 +8,18 @@ print('. '.join(new_list).title())
 print('Random word from list of your written words: ', r.choice(new_list))
 print('Your sorted list: ', sorted(new_list))
 print('Your list converted to dictionary with numeric keys: ', {i: new_list[i] for i in range(len(new_list))})
+
+string = 'Hello my friend, my name is Dima.'
+new_string = string.replace('Dima', 'Yoshikage Kira')
+print(new_string)
+
+
+# I love this function, recursion is really fine thing
+def find_factorial(num):
+    if num == 0:
+        return 1
+    else:
+        return num * find_factorial(num-1)
+
+
+print(find_factorial(5))
